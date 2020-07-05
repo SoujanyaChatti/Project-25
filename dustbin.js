6627505d776b5128aba30805d25b41582bb3e609
SoujanyaChatti <67154494+SoujanyaChatti@users.noreply.github.com>
2020-07-05 20:18:40 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,8 @@
+class Box extends BaseClass{
+constructor(x,y,width,height){
+   super(x,y,170,170);
+  
+    //this.image=loadImage("sprites/bin.png");
+    
+}
+}
---
+++ paper.js
@@ -0,0 +1,10 @@
+class Paper extends BaseClass{
+    constructor(x,y,width,height){
+        super(x,y,width,height);
+        this.image=loadImage("sprites/paperim.png");
+        
+    }
+   
+
+    }
+   
